d559cb6c56f1e15f0fcf4fdf9db85a72486c5a2a
David <daviderophillips@gmail.com>
2025-02-23 14:36:39 +0100

    done with the hero section for all screens

---
+++ script.js
@@ -0,0 +1,8 @@
+// get hold of barsicon div that contains the icon
+
+document.querySelector(".barsicon").addEventListener("click", function() {
+    const navlinks = document.querySelector("nav");
+    navlinks.classList.toggle("show")
+});
+
+
