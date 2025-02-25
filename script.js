f2db5fe26f2c253d9448b590f79e13a1f8e3eee5
David <daviderophillips@gmail.com>
2025-02-25 14:28:07 +0100

    done with the offers section across the screens

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ document.querySelector(".barsicon").addEventListener("click", function() {
     navlinks.classList.toggle("show")
 });
 
+// OFFERS GRID FUNCTIONS - MODALS
+
+// A:
 // Get the modal
 const modal = document.getElementById("myModal");
 
@@ -26,3 +29,68 @@ spanClose.onclick = function() {
   modal.style.display = "none";
 }
 
+// B:
+// Get the modal
+const modalB = document.getElementById("myModalB");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+const imgB = document.getElementById("myImgB");
+const modalImgB = document.getElementById("img01B");
+const captionTextB = document.getElementById("captionB");
+imgB.onclick = function() {
+  modalB.style.display = "block";
+  modalImgB.src = this.src;
+  captionTextB.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+const spanCloseB = document.getElementsByClassName("closeB")[0];
+
+// When the user clicks on <span> (x), close the modal
+spanCloseB.onclick = function() {
+  modalB.style.display = "none";
+}
+
+// C:
+// Get the modal
+const modalC = document.getElementById("myModalC");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+const imgC = document.getElementById("myImgC");
+const modalImgC = document.getElementById("img01C");
+const captionTextC = document.getElementById("captionC");
+imgC.onclick = function() {
+  modalC.style.display = "block";
+  modalImgC.src = this.src;
+  captionTextC.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+const spanCloseC = document.getElementsByClassName("closeC")[0];
+
+// When the user clicks on <span> (x), close the modal
+spanCloseC.onclick = function() {
+  modalC.style.display = "none";
+}
+
+// D:
+// Get the modal
+const modalD = document.getElementById("myModalD");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+const imgD = document.getElementById("myImgD");
+const modalImgD = document.getElementById("img01D");
+const captionTextD = document.getElementById("captionD");
+imgD.onclick = function() {
+  modalD.style.display = "block";
+  modalImgD.src = this.src;
+  captionTextD.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+const spanCloseD = document.getElementsByClassName("closeD")[0];
+
+// When the user clicks on <span> (x), close the modal
+spanCloseD.onclick = function() {
+  modalD.style.display = "none";
+}
