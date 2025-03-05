ac9ae9463b5978ce6905166262ab31622c3d0fc4
Ayodeji <thimmzieayodeji@gmail.com>
2025-03-05 19:41:22 +0100

    about-page

--- about-us/about.js
+++ about-us/about.js
@@ -1,14 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const navBar = document.querySelector(".site-navigations");
 const overlay = document.querySelector(".box-overlay");
-const unscroll = document.querySelector(".unscroll");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navBar.classList.toggle("active");
   document.body.style.height = "100vh";
   overlay.classList.toggle("active");
-  document.body.classList.add("unscroll");
 });
 
 document.querySelectorAll(".nav-link").forEach((n) =>
@@ -17,6 +15,5 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     navBar.classList.remove("active");
     overlay.classList.remove("active");
     unscroll.classList.remove("active");
-    document.body.classList.remove("unscroll");
   })
 );
